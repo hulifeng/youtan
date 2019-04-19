dacc7eb9700352eb62ef3835ac650ec9e31721aa
mason <9265959@qq.com>
2019-04-19 15:10:48 +0800

    话题列表，修复N+1问题

---
+++ app/Http/Controllers/TopicsController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Topic;
+use Illuminate\Http\Request;
+
+class TopicsController extends Controller
+{
+    public function index()
+    {
+        $topics = Topic::with(['category', 'user'])->paginate();
+        return view('topics.index', compact('topics'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,5 @@
 <?php
-Route::get('/', 'PagesController@root')->name('root');
+Route::get('/', 'TopicsController@index')->name('root');
 Auth::routes(['verify' => true]);
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'edit', 'update']]);
