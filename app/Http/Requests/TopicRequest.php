2b35094fb34ba6c4f25b3f442b9f0f118683be06
mason <9265959@qq.com>
2019-05-05 18:04:32 +0800

    增加标签功能

--- app/Http/Requests/TopicRequest.php
+++ app/Http/Requests/TopicRequest.php
@@ -18,6 +18,7 @@ class TopicRequest extends Request
                         'title'       => 'required|min:2',
                         'markdown_content'        => 'required|min:3',
                         'category_id' => 'required|numeric',
+                        'tags' => 'required'
                     ];
                 }
             case 'GET':
@@ -32,8 +33,12 @@ class TopicRequest extends Request
     public function messages()
     {
         return [
+            'title.required' => '标题不能为空',
             'title.min' => '标题必须至少两个字符',
+            'markdown_content.required' => '文章内容不能为空',
             'markdown_content.min' => '文章内容必须至少三个字符',
+            'category_id.required' => '请选择分类',
+            'tags.required' => '标签不能为空'
         ];
     }
 }
--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -2,29 +2,31 @@
 
 @section('afterCss')
     @include('Smartmd::head')
+    <link rel="stylesheet" href="/assets/taginput/jquery.tagsinput.css">
 @endsection
 
 @section('title', $topic->id ? '编辑文章' : '新建文章')
 
 @section('content')
     <div class="container" style="max-width: 1000px;">
+        <h4 style="margin-bottom: 20px; color: #9199a1;"><i class="fas fa-pencil-alt"></i> 文章发布</h4>
+        @if($topic->id)
+            <form action="{{ route('topics.update', $topic->id) }}" class="form-horizontal" method="post" enctype="multipart/form-data" accept-charset="UTF-8">
+                <input type="hidden" name="_method" value="PUT">
+                @else
+                    <form action="{{ route('topics.store') }}" method="post" class="form-horizontal" enctype="multipart/form-data" accept-charset="UTF-8">
+                        @endif
+                        {{ csrf_field() }}
         <div class="blog-write">
             <div class="row">
                 <div class="col-md-12">
-                    @if($topic->id)
-                        <form action="{{ route('topics.update', $topic->id) }}" class="form-horizontal" method="post" enctype="multipart/form-data" accept-charset="UTF-8">
-                            <input type="hidden" name="_method" value="PUT">
-                    @else
-                        <form action="{{ route('topics.store') }}" method="post" class="form-horizontal" enctype="multipart/form-data" accept-charset="UTF-8">
-                    @endif
-                        {{ csrf_field() }}
 
                         @include('shared._error')
 
                         <div class="form-group row">
                             <label for="title" class="col-sm-1 col-form-label">标题</label>
                             <div class="col-sm-6">
-                                <input type="text" name="title" id="title" class="form-control" value="{{ old('title', $topic->title) }}" placeholder="请填写标题" required style="width: 100%; margin-bottom: 20px;">
+                                <input type="text" name="title" id="title" class="form-control" value="{{ old('title', $topic->title) }}" placeholder="请填写标题" style="width: 100%; margin-bottom: 20px;">
                             </div>
                         </div>
 
@@ -42,7 +44,6 @@
                             </div>
                         </div>
 
-                    </form>
                 </div>
             </div>
         </div>
@@ -53,8 +54,9 @@
                         <label for="category_id" class="col-sm-1 col-form-label">分类</label>
                         <div class="col-sm-11">
                             <div class="categories">
+                                <input type="hidden" name="category_id" id="category_id">
                                 @foreach($categories as $category)
-                                    <div class="category-item">{{ $category->name }}</div>
+                                    <div class="category-item" data-id="{{ $category->id }}">{{ $category->name }}</div>
                                 @endforeach
                             </div>
                         </div>
@@ -63,18 +65,21 @@
                     <div class="form-group row">
                         <label for="tags" class="col-sm-1 col-form-label">标签</label>
                         <div class="col-sm-11">
-                            <input type="text" class="form-control" name="tags" id="tags" placeholder="回车键确定输入标签">
+                            <div id="tags"></div>
+                            <input type="text" id="tag-input" class="form-control" name="tags">
                         </div>
                     </div>
 
-                    <center><button class="btn btn-primary" style="margin-top: 20px; color: #fff; margin-right: 20px;"><i class="fas fa-save"></i> 保存草稿</button><button class="btn bg-green" style="color: #fff; margin-top: 20px;"><i class="fa fa-paper-plane"></i> 发布文章</button></center>
+                    <center><button class="btn btn-primary" style="margin-top: 20px; color: #fff; margin-right: 20px;"><i class="fas fa-save"></i> 保存草稿</button><button type="submit" class="btn bg-green" style="color: #fff; margin-top: 20px;"><i class="fa fa-paper-plane"></i> 发布文章</button></center>
                 </div>
             </div>
         </div>
+        </form>
     </div>
 @endsection
 
 @section('afterJs')
+    <script src="/assets/taginput/jquery.tagsinput.js"></script>
     <script>
         new Smartmd({
             el: "#editor",
@@ -98,5 +103,27 @@
         document.addEventListener('paste', function (event) {
             console.log(event)
         });
+
+        $(function () {
+            $('.category-item').click(function () {
+                var category_id = $(this).attr('data-id');
+                $(this).siblings().removeClass('active');
+                $(this).addClass('active');
+                $('#category_id').val(category_id);
+            });
+            $('#tag-input').tagsInput({
+                placeholder: '按Tab键增加标签，最多添加五个标签',
+                limit: 5,
+                'onAddTag': function(input, value) {
+                    console.log('tag added', input, value);
+                },
+                'onRemoveTag': function(input, value) {
+                    console.log('tag removed', input, value);
+                },
+                'onChange': function(input, value) {
+                    console.log('change triggered', input, value);
+                }
+            });
+        })
     </script>
 @endsection
